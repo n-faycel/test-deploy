05bd3989239507d43d34dbd21735d11e2631296b
Nasmi Faycel <faycel.nasmi@outlook.fr>
2023-03-30 15:44:06 +0200

    :rocket: test-cd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with Hamza lo</h1>
+          <h1>Wilders Book with Hamza loance</h1>
         </Container>
       </Header>
       <Container>
